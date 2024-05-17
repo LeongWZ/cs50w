cb6a2b4650b0c80b3e193896c3f8d02fc78c7ea3
LeongWZ <leongweizhi03@gmail.com>
2024-05-18 00:58:14 +0800

    Update mail project

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -24,6 +24,8 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  document.querySelectorAll("#error-div").forEach(element => element.remove());
 }
 
 function load_mailbox(mailbox) {
@@ -35,9 +37,6 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-
-  // Hide error alert
-  document.querySelector("#error-div").style.display = "none";
 
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
@@ -96,12 +95,20 @@ function send_mail() {
     return result;
   })
   .then(result => {
-    document.querySelector("#error-div").style.display = "none";
+    document.querySelectorAll("#error-div").forEach(element => element.remove());
     load_mailbox("sent");
   })
   .catch(error => {
-    document.querySelector("#error-div").style.display = "block";
-    document.querySelector("#error-alert").innerHTML = `Error: ${error.message}`;
+    document.querySelectorAll("#error-div").forEach(element => element.remove());
+
+    const error_div = document.createElement("div");
+    error_div.id = "error-div";
+    error_div.innerHTML = `
+      <div class="alert alert-danger" id="error-alert">
+        Error: ${error.message}
+      </div>
+    `;
+    document.querySelector("#compose-view").append(error_div);
   });
   
   // stop form from submitting
@@ -137,22 +144,11 @@ function create_email_preview_card(email, belongs_to_sent_view) {
 
   const card_body = document.createElement("div");
   card_body.className = "card-body";
-  email_preview_card.append(card_body);
-
-  const sender_element = document.createElement("h5");
-  sender_element.innerHTML = `${email?.sender}`;
-  sender_element.className = "card-title";
-  card_body.append(sender_element);
-
-  const subject_element = document.createElement("p");
-  subject_element.innerHTML = `${email?.subject}`;
-  subject_element.className = "card-text";
-  card_body.append(subject_element);
-
-  const timestamp_element = document.createElement("h6");
-  timestamp_element.innerHTML = `${email?.timestamp}`;
-  timestamp_element.className = "card-subtitle text-muted";
-  card_body.append(timestamp_element);
+  card_body.innerHTML = `
+    <h5 class="card-title">${email?.sender}</h5>
+    <p class="card-text">${email?.subject}</p>
+    <h6 class="card-subtitle text-muted">${email?.timestamp}</h6>
+  `;
 
   const link_element = document.createElement("a");
   link_element.className = "stretched-link";
@@ -161,16 +157,19 @@ function create_email_preview_card(email, belongs_to_sent_view) {
   // spread link over to card_body only
   card_body.style= "transform: rotate(0);";
 
+  email_preview_card.append(card_body);
+
   if (!belongs_to_sent_view) {
     const card_footer = document.createElement("div");
     card_footer.className = "card-footer";
-    email_preview_card.append(card_footer);
 
     const archive_button = document.createElement("button");
     archive_button.className = "btn btn-primary btn-sm";
     archive_button.innerText = email?.archived ? "Unarchive" : "Archive";
     archive_button.onclick = () => archive_mail(email, !email.archived);
     card_footer.append(archive_button);
+
+    email_preview_card.append(card_footer);
   }
   
   return email_preview_card;
@@ -187,33 +186,17 @@ function create_email_card(email, belongs_to_sent_view) {
 
   const card_body = document.createElement("div");
   card_body.className = "card-body";
+  card_body.innerHTML = `
+    <h5 class="card-title">${email?.sender}</h5>
+    <h6 class="card-subtitle text-muted">${email?.timestamp}</h6>
+    <div class="card-text mt-2">To: ${email?.recipients}</div>
+    <hr/>
+    <p>${email?.body}</p>
+  `;
   email_card.append(card_body);
-
-  const sender_element = document.createElement("h5");
-  sender_element.innerHTML = `${email?.sender}`;
-  sender_element.className = "card-title";
-  card_body.append(sender_element);
-
-  const timestamp_element = document.createElement("h6");
-  timestamp_element.innerHTML = `${email?.timestamp}`;
-  timestamp_element.className = "card-subtitle text-muted";
-  card_body.append(timestamp_element);
-
-  const recipients_element = document.createElement("div");
-  recipients_element.innerHTML = `To: ${email?.recipients}`;
-  recipients_element.className = "card-text mt-2";
-  card_body.append(recipients_element);
-
-  card_body.append(document.createElement("hr"));
-
-  const body_element = document.createElement("p");
-  body_element.innerText = email?.body;
-  body_element.className = "card-text mt-3";
-  card_body.append(body_element);
 
   const card_footer = document.createElement("div");
   card_footer.className = "card-footer";
-  email_card.append(card_footer);
 
   if (!belongs_to_sent_view) {
     const archive_button = document.createElement("button");
@@ -222,12 +205,13 @@ function create_email_card(email, belongs_to_sent_view) {
     archive_button.onclick = () => archive_mail(email, !email.archived);
     card_footer.append(archive_button);
   }
-
   const reply_button = document.createElement("button");
   reply_button.className = "btn btn-primary btn-sm";
   reply_button.innerText = "Reply";
   reply_button.onclick = () => reply_mail(email);
   card_footer.append(reply_button);
+
+  email_card.append(card_footer);
   
   return email_card;
 }
